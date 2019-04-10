28424cfe5aa3349412f81f958998aa1b8bca4c9b
saleall <gebi9@hotmail.com>
2019-04-10 23:14:36 +0200

    app complete.

--- src/components/front-end/table/TableView.jsx
+++ src/components/front-end/table/TableView.jsx
@@ -2,17 +2,116 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/TableView.css';
 import {
-    ButtonToolbar, Navbar, Row, Col, Table, Button, Tooltip, FormGroup,
-    InputGroup, FormControl, OverlayTrigger, ButtonGroup, ProgressBar, TableRow
+    ButtonToolbar, Table, Button
 } from 'react-bootstrap';
 
 class TableView extends Component {
-    state = {}
-
-    //reload the api to update current status
-    updateStatus = () => {
-        console.log('////update///');
+    rowNrID = 0;
+    isUpdating = false; // flag to prevent rapid update requests.
+    // JSON files from fetch
+    stationListJSON = null;
+    stationStatusListJSON = null;
+    state = {
+        // combined informations ready to view
+        listOfTableDataConstructedFromJSON: []
     }
+
+    // lifecycle hook that will run everytime this component is reloaded/starts
+    componentDidMount = () => {
+        this.getDataFromAPI();
+    }
+
+    //fetchs data from two API requestes
+    getDataFromAPI = () => {
+        if (this.isUpdating) {
+            return;
+        } else {
+            this.isUpdating = true; //update start
+        }
+        //fetch station property data needed from the API
+        let fetchStationsInfo = () => {
+            return new Promise((resolve, reject) => {
+                fetch('https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json', {
+                    header: { "Client-Identifier": "private-monitorapp" }
+                }).then(response => {
+                    return response.json();
+                }).then(JSONdata => {
+                    this.stationListJSON = JSONdata;
+                    resolve();
+                }).catch(error => {
+                    reject('Stasjonenes informasjon er ikke tilgjengelig for øyebliket! \n Vennligst prøv igjen senere', '\n\n', error);
+                });
+            });
+        }
+        //then fetch station status data needed from the API
+        let fetchStationsCurrentStatus = () => {
+            return new Promise((resolve, reject) => {
+                fetch('https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json', {
+                    header: { "Client-Identifier": "private-monitorapp" }
+                }).then(response => {
+                    return response.json();
+                }).then(JSONdata => {
+                    this.stationStatusListJSON = JSONdata;
+                    resolve();
+                }).catch(error => {
+                    alert('Stasjonenes status informasjon er ikke tilgjengelig for øyebliket! \n Vennligst prøv igjen senere', '\n\n', error);
+                    reject();
+                });
+            });
+        }
+        //actions are then guaranteed to go asynchronously!
+        fetchStationsInfo().then(() => {
+            return fetchStationsCurrentStatus();
+        }).then(() => {
+            this.extractViewDataFromJSON();
+        });
+    }
+
+    //extracts the relevant data to be viewd inside the table.
+    extractViewDataFromJSON = () => {
+        const listOfStationsProperty = this.stationListJSON.data.stations;
+        const listOfStationsStatus = this.stationStatusListJSON.data.stations;
+        let iterationLength = 0;
+        let isStationsListAPIactive = false;
+        let isStationsStatusListAPIactive = false;
+        if (listOfStationsProperty != null) {
+            iterationLength = listOfStationsProperty.length;
+            isStationsListAPIactive = true;
+        }
+        if (listOfStationsStatus != null) {
+            iterationLength = listOfStationsStatus.length;
+            isStationsStatusListAPIactive = true;
+        }
+        if ((listOfStationsProperty == null) && (listOfStationsStatus == null)) {
+            alert('Informasjonene er ikke tilgjengelige for øyebliket!\n Vennligst prøv igjen senere.');
+            return; // terminate
+        }
+        let tempArrayForTableData = [];
+        for (let i = 0; i < iterationLength; i++) {
+            let nameOfStation = (isStationsListAPIactive) ? listOfStationsProperty[i].name : 'Ikke tilgjengelig';
+            let availableDocks = 'Ikke tilgjengelig';
+            let availableBikes = 'Ikke tilgjengelig';
+            if (isStationsStatusListAPIactive) {
+                availableDocks = listOfStationsStatus[i].num_docks_available;
+                availableBikes = listOfStationsStatus[i].num_bikes_available;
+            }
+            tempArrayForTableData.push(
+                {
+                    stationName: nameOfStation,
+                    numberOfBikesAvailable: availableBikes,
+                    numberOfDocksAvailable: availableDocks
+                }
+            );
+        }
+        //updates view data on reaload
+        this.setState({
+            listOfTableDataConstructedFromJSON: tempArrayForTableData
+        });
+        this.isUpdating = false; //update done
+        console.log('////updated////');
+    }
+
+    ///render view
     render() {
         return (
             <div className="gridprimaryContainer container">
@@ -23,11 +122,10 @@ class TableView extends Component {
                 </div>
                 <div className="bg-light py-1">
                     <ButtonToolbar>
-                        <Button variant="secondary" block onClick={this.updateStatus}> Oppdater </Button>
+                        <Button variant="secondary" block onClick={this.getDataFromAPI}> Oppdater </Button>
                     </ButtonToolbar>
                 </div>
-
-                <Table striped bordered hover>
+                <Table striped bordered hover >
                     <thead>
                         <tr>
                             <th>Stasjon</th>
@@ -36,21 +134,17 @@ class TableView extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Oslo s nord</td>
-                            <td>23</td>
-                            <td>12</td>
-                        </tr>
-                        <tr>
-                            <td>Forskningsparken</td>
-                            <td>0</td>
-                            <td>25</td>
-                        </tr>
+                        {this.state.listOfTableDataConstructedFromJSON.map(row => (
+                            <tr key={this.rowNrID++} >
+                                <td>{row.stationName}</td>
+                                <td>{row.numberOfDocksAvailable}</td>
+                                <td>{row.numberOfBikesAvailable}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </Table>
             </div>
         );
     }
 }
-
 export default TableView;
