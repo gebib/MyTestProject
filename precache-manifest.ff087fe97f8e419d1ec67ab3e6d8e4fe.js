1d6e1a5523ba350ba41feaf17655b61580263b74
saleall <gebi9@hotmail.com>
2019-04-11 09:45:50 +0200

    Updates

---
+++ precache-manifest.ff087fe97f8e419d1ec67ab3e6d8e4fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ca9c739267488d06d98",
+    "url": "/MyTestProject/static/js/runtime~main.eb45ec40.js"
+  },
+  {
+    "revision": "2b8f946aec9d6ad2390b",
+    "url": "/MyTestProject/static/js/main.fa19cedd.chunk.js"
+  },
+  {
+    "revision": "478f3b4c479f2bfbce77",
+    "url": "/MyTestProject/static/js/2.897d1a6e.chunk.js"
+  },
+  {
+    "revision": "2b8f946aec9d6ad2390b",
+    "url": "/MyTestProject/static/css/main.7405fb23.chunk.css"
+  },
+  {
+    "revision": "478f3b4c479f2bfbce77",
+    "url": "/MyTestProject/static/css/2.0f0d8340.chunk.css"
+  },
+  {
+    "revision": "97ad51c5a84927096f28af9ea4e4cd4b",
+    "url": "/MyTestProject/index.html"
+  }
+];
